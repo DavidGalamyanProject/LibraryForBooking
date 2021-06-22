bdc0b2dd5bcf23d6ad188cc08b6208b3c76a22b7
DavidGalamyan <DavidGalamyan@gmail.com>
2021-06-22 18:32:55 +0300

    Переработать метод добавления товара

--- ShopWebApi/Domain/Accounting.cs
+++ ShopWebApi/Domain/Accounting.cs
@@ -1,35 +1,43 @@
 ﻿using ShopWebApi.Model.Dto;
 using System.Collections.Concurrent;
+using System.Collections.Immutable;
 
 namespace ShopWebApi.Domain
 {
 	/// <summary> Списки учета, с их помощью снимается нагрузка с БД </summary>
 	internal class Accounting
-    {
+	{
 
-        private static readonly object _lock = new object();
-        private static readonly object _lock2 = new object();
-        private static ConcurrentQueue<ReservRequestToQueue> _reservRequestToQueues;
-        private Accounting() { }
-        /// <summary> Реализация многопоточного Singlton </summary>
-        internal static ConcurrentQueue<ReservRequestToQueue> RequestReservQueue {
-            get
-            {
-                if (_reservRequestToQueues == null)
-                {
-                    lock (_lock)
-                    {
-                        if (_reservRequestToQueues == null)
-                        {
-                            _reservRequestToQueues = new ConcurrentQueue<ReservRequestToQueue>();
-                        }
-                    }
-                }
-                return _reservRequestToQueues;
-            }
-            set
-			{								
+		private static readonly object _lock = new object();
+		private static ConcurrentQueue<ReservRequestToQueue> _reservRequestToQueues;
+		private Accounting() { }
+		/// <summary> Реализация многопоточного Singlton </summary>
+		public static void Add(ReservRequestToQueue item)
+		{
+			if (_reservRequestToQueues == null)
+			{
+				lock (_lock)
+				{
+					if (_reservRequestToQueues == null)
+					{
+						_reservRequestToQueues = new ConcurrentQueue<ReservRequestToQueue>();
+					}					
+				}				
 			}
-        }
-    }
+			lock (_lock)
+			{
+				_reservRequestToQueues.Enqueue(item);
+			}
+		}
+		/// <summary> Простите я даун, не как сделать лучше! </summary>
+		public static ImmutableList<ReservRequestToQueue> GetAllReservs()
+		{
+			lock(_lock)
+			{
+				var result = _reservRequestToQueues.ToImmutableList();
+				_reservRequestToQueues.Clear();
+				return result;
+			}
+		}
+	}
 }
