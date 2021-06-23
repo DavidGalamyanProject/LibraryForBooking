3fec5f91924e5530e57c43ee6d12537194dfdf8d
DavidGalamyan <74913694+DavidGalamyan@users.noreply.github.com>
2021-06-23 22:23:29 +0300

    Update Accounting.cs

--- ShopWebApi/Domain/Accounting.cs
+++ ShopWebApi/Domain/Accounting.cs
@@ -1,17 +1,21 @@
-﻿using ShopWebApi.Model.Dto;
+using ShopWebApi.Model.Dto;
 using System.Collections.Concurrent;
 using System.Collections.Immutable;
 
 namespace ShopWebApi.Domain
 {
-	/// <summary> Списки учета, с их помощью снимается нагрузка с БД </summary>
+	/// <summary> Списки учета, с их помощью снимается нагрузка с БД 
+	/// Поверх конкурентной очереди, реализованная своя система лока
+	/// Потому что, когда необходимо получить из очереди лист обектов
+	/// Мы должны быть уверены, что другие потоки в данный момент, коллекция не используется
+	/// и будет доступна только после действия!</summary>
 	internal class Accounting
 	{
 
 		private static readonly object _lock = new object();
-		private static ConcurrentQueue<ReservRequestToQueue> _reservRequestToQueues;
+		private static ConcurrentQueue<ReservRequestToQueue> _reservRequestToQueues;		
 		private Accounting() { }
-		/// <summary> Реализация многопоточного Singlton </summary>
+		/// <summary> Метод добавление, в очередь </summary>
 		public static void Add(ReservRequestToQueue item)
 		{
 			if (_reservRequestToQueues == null)
@@ -29,9 +33,20 @@ namespace ShopWebApi.Domain
 				_reservRequestToQueues.Enqueue(item);
 			}
 		}
+		/// <summary> Метод получения списка объекта из очереди </summary>
 		public static ImmutableList<ReservRequestToQueue> GetAllReservs()
 		{
-			lock(_lock)
+			if (_reservRequestToQueues == null)
+			{
+				lock (_lock)
+				{
+					if (_reservRequestToQueues == null)
+					{
+						_reservRequestToQueues = new ConcurrentQueue<ReservRequestToQueue>();
+					}
+				}
+			}
+			lock (_lock)
 			{
 				var result = _reservRequestToQueues.ToImmutableList();
 				_reservRequestToQueues.Clear();
